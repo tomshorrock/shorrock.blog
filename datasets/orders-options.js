0c08612b99936a822627a07969f8e2d878d0b398
Tom Shorrock <Tom.Shorrock@forwoodsafety.com>
2019-05-18 14:30:09 +0100

    Site updated: 2019-05-18 14:30:09

---
+++ datasets/orders-options.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    name: 'AUD',
+    strike: 70,
+    date: '',
+    expiry: '2020-03-06',
+    price: 2.053,
+    amount: 1,
+  },
+  {
+    name: 'AUD',
+    strike: 61,
+    date: '',
+    expiry: '2020-03-06',
+    price: 0.273,
+    amount: 4,
+  },
+  {
+    name: 'AUD',
+    strike: 64,
+    date: '',
+    expiry: '2020-03-06',
+    price: 0.083,
+    amount: 8,
+  },
+];
